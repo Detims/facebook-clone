9f293f601320e621f60832b26e1fc368fe60d281
Detims <2005.nnhan@gmail.com>
2025-10-27 23:15:54 -0700

    implement header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,15 +1,74 @@
 import Image from "next/image";
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { RiFlag2Line } from "react-icons/ri";
+import { MdOndemandVideo, MdOutlineExpandMore } from "react-icons/md";
+import { AiOutlineShop, AiFillMessage, AiFillBell } from "react-icons/ai";
+import { IoGameControllerOutline, IoHomeOutline } from "react-icons/io5";
+import { CgMenuGridO } from "react-icons/cg";
 
 const Header = () => {
     return(
         <div className="bg-white flex items-center p-2 shadow-md top-0 sticky z-50 h-16">
-            <div className="flex min-w-fit">
+            {/* Icon and Search */}
+            <nav className="flex min-w-fit">
                 <Image 
                     src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg" 
                     width={40}
                     height={40}
                     alt="logo"
+                />
+                <div className="flex items-center space-x-2 px-2 ml-2 rounded-full bg-gray-100 text-gray-500">
+                    <FaSearch size={20} />
+                    <input className="hidden lg:inline-flex bg-transparent focus:outline-none" type="text" placeholder="Search Facebook" />
+                </div>
+            </nav>
+            
+            <div className="flex grow justify-center mx-2">
+                <div className="flex items-center">
+                    <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer">
+                        <IoHomeOutline className="mx-auto" size={25}/>
+                    </div>
+                    <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer">
+                        <RiFlag2Line className="mx-auto" size={25}/>
+                    </div>
+                    <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer">
+                        <MdOndemandVideo className="mx-auto" size={25}/>
+                    </div>
+                    <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer">
+                        <AiOutlineShop className="mx-auto" size={25}/>
+                    </div>
+                    <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer">
+                        <IoGameControllerOutline className="mx-auto" size={25}/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex items-center justify-end min-w-fit space-x-2">
+                <Image 
+                    src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg" 
+                    width={40}
+                    height={40}
+                    alt="logo"
+                />
+                <p className="hidden xl:inline-flex font-semibold text-sm whitespace-nowrap p-3 max-w-xs">
+                    Some name
+                </p>
+                <CgMenuGridO 
+                    className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300" 
+                    size={20} 
+                />
+                <AiFillMessage 
+                    className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300" 
+                    size={20} 
+                />
+                <AiFillBell 
+                    className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300" 
+                    size={20} 
+                />
+                <MdOutlineExpandMore 
+                    className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300" 
+                    size={20} 
                 />
             </div>
         </div>
