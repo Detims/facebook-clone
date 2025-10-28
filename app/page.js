5ba935dcf336c3f62e7ee661ed09160724e08a02
Detims <2005.nnhan@gmail.com>
2025-10-28 00:06:38 -0700

    add sidebar elements

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
-        <Header />
+    <div>
+      <Header />
+      <main className="flex bg-gray-100">
+        <Sidebar />
+      </main>
     </div>
   );
 }
